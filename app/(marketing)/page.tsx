45d48ca3a7a291fa5d55f0721b248606534199cf
DonaldKisaka <kisakadonald1@gmail.com>
2025-11-23 07:40:01 +0300

    Homepage

---
+++ app/(marketing)/page.tsx
@@ -0,0 +1,11 @@
+const homePage = () => {
+  return (
+    <div>
+      <h1>
+        My name is Donald Kisaka!
+      </h1>
+    </div>
+  )
+}
+
+export default homePage
---
+++ components/Footer.tsx
@@ -0,0 +1,17 @@
+
+const Footer = () => {
+    const currentYear = new Date().getFullYear();
+  return (
+    <footer className="border-t border-gray-200">
+        <div className="max-w-7xl mx-auto px-4 py-8 sm:px-6 lg:px-8">
+            <div className="text-center text-sm text-gray-500">
+                <p>
+                    &copy; {currentYear} Loopuni. All rights reserved.
+                </p>
+            </div>
+        </div>
+    </footer>
+  )
+}
+
+export default Footer;  
